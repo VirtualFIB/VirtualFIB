28c3872dff71fca13b6b03f6783784f3664af2c1
Aleksander B. Mosberg <aleksander.b.mosberg@gmail.com>
2021-03-09 12:43:43 +0000

    Pared down __init__

--- __init__.py
+++ __init__.py
@@ -18,56 +18,71 @@ import bpy
 from bpy.app.handlers import persistent
 from . import loa, stagesim, postwelder
 
-@persistent
-def load_handler_for_preferences(_):
-    print("Changing Preference Defaults!")
-    from bpy import context
+# @persistent
+# def load_handler_for_preferences(_):
+#     print("Changing Preference Defaults!")
+#     from bpy import context
 
-    prefs = context.preferences
-    prefs.use_preferences_save = False
+#     prefs = context.preferences
+#     prefs.use_preferences_save = False
 
-    kc = context.window_manager.keyconfigs["blender"]
-    kc_prefs = kc.preferences
-    if kc_prefs is not None:
-        kc_prefs.select_mouse = 'LEFT'
-        kc_prefs.spacebar_action = 'SEARCH'
-        kc_prefs.use_pie_click_drag = True
+#     kc = context.window_manager.keyconfigs["blender"]
+#     kc_prefs = kc.preferences
+#     if kc_prefs is not None:
+#         kc_prefs.select_mouse = 'LEFT'
+#         kc_prefs.spacebar_action = 'SEARCH'
+#         kc_prefs.use_pie_click_drag = True
 
-    view = prefs.view
-    view.header_align = 'BOTTOM'
+#     view = prefs.view
+#     view.header_align = 'BOTTOM'
 
 
-@persistent
-def load_handler_for_startup(_):
-    print("Changing Startup Defaults!")
+# @persistent
+# def load_handler_for_startup(_):
+#     print("Changing Startup Defaults!")
 
-    # Use smooth faces.
-    for mesh in bpy.data.meshes:
-        for poly in mesh.polygons:
-            poly.use_smooth = False
+#     # Use smooth faces.
+#     for mesh in bpy.data.meshes:
+#         for poly in mesh.polygons:
+#             poly.use_smooth = False
 
-    # Use material preview shading.
-    for screen in bpy.data.screens:
-        for area in screen.areas:
-            for space in area.spaces:
-                if space.type == 'VIEW_3D':
-                    space.shading.type = 'MATERIAL'
-                    space.shading.use_scene_lights = True
+#     # Use material preview shading.
+#     for screen in bpy.data.screens:
+#         for area in screen.areas:
+#             for space in area.spaces:
+#                 if space.type == 'VIEW_3D':
+#                     space.shading.type = 'MATERIAL'
+#                     space.shading.use_scene_lights = True
+
+modules = (
+    loa,
+    stagesim,
+    postwelder
+    )
 
 
 def register():
-    print("Registering to Change Defaults")
-    bpy.app.handlers.load_factory_preferences_post.append(load_handler_for_preferences)
-    bpy.app.handlers.load_factory_startup_post.append(load_handler_for_startup)
-    loa.register()
-    stagesim.register()
-    postwelder.register()
+    # print("Registering to Change Defaults")
+    # bpy.app.handlers.load_factory_preferences_post.append(load_handler_for_preferences)
+    # bpy.app.handlers.load_factory_startup_post.append(load_handler_for_startup)
+    for module in modules:
+        module.register()
+
+    # loa.register()
+    # stagesim.register()
+    # postwelder.register()
 
 
 def unregister():
-    print("Unregistering to Change Defaults")
-    bpy.app.handlers.load_factory_preferences_post.remove(load_handler_for_preferences)
-    bpy.app.handlers.load_factory_startup_post.remove(load_handler_for_startup)
-    postwelder.unregister()
-    stagesim.unregister()
-    loa.unregister()
+    # print("Unregistering to Change Defaults")
+    # bpy.app.handlers.load_factory_preferences_post.remove(load_handler_for_preferences)
+    # bpy.app.handlers.load_factory_startup_post.remove(load_handler_for_startup)
+    for module in modules[::-1]:
+        module.unregister()
+
+    # postwelder.unregister()
+    # stagesim.unregister()
+    # loa.unregister()
+
+if __name__ == "__main__":
+    register()
